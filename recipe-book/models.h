571df3e7d29749d2c0a1141f20b7b75a92ec1175
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2025-07-21 13:57:34 -0400

    Updated documentation and added header only header.
    Added the models.h that groups up relevant headers to ease imports.

---
+++ recipe-book/models.h
@@ -0,0 +1,14 @@
+#ifndef MODELS_H
+#define MODELS_H
+
+#include "models/qingredient.h"
+#include "models/qprofile.h"
+#include "models/qrecipe.h"
+#include "models/qrecipeingredient.h"
+
+#include "models/ingredientfactory.h"
+#include "models/profilefactory.h"
+#include "models/recipefactory.h"
+#include "models/recipeingredientfactory.h"
+
+#endif
