907ed0e6a56438396000a29e9da9b8d613acc912
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2025-11-25 02:11:52 -0500

    Fixed typo in .clang-tidy filename
    Updated CMakeLists to include clang-format and clang-tidy linter scripts.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "utils/configmanager.h"
 #include "utils/loggingmanager.h"
-#include "utils/uuid.h"
 
 #include <gtkmm-4.0/gtkmm.h>
 
@@ -25,10 +24,6 @@ int main(int argc, char **argv) {
   ConfigManager::init();
 
   g_info("Software successfully configured and ready to run");
-
-  std::string uuid = Uuid::generate();
-  g_info("Generated uuid: %s", uuid.c_str());
-  g_info("Uuid %s checked for validity: %d", uuid.c_str(), Uuid::isValid(uuid));
 
   int result = app->make_window_and_run<MainWindow>(argc, argv);
 
